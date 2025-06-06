6b1d33a557d222694759b44a5659d3f668da0532
phreemason <mr.omarmason@gmail.com>
2025-06-05 22:19:59 -0400

    rearrange explore tab in TabLayout putting search after library

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -42,17 +42,17 @@ export default function TabLayout() {
                 }}
             />
             <Tabs.Screen
-                name="explore"
-                options={{
-                    title: 'Explore',
-                    tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-                }}
-            />
-            <Tabs.Screen
                 name="search"
                 options={{
                     title: 'Search',
                     tabBarIcon: ({ color }) => <IconSymbol size={28} name="magnifyingglass" color={color} />,
+                }}
+            />
+             <Tabs.Screen
+                name="explore"
+                options={{
+                    title: 'Explore',
+                    tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
                 }}
             />
         </Tabs>
