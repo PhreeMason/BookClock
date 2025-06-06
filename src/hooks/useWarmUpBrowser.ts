2d8cb2d6b7b3f89c27bd5d7c1271a30dc13e21e0
phreemason <mr.omarmason@gmail.com>
2025-06-05 22:10:36 -0400

    update useWarmUpBrowser hook to conditionally warm up the browser only on Android

--- src/hooks/useWarmUpBrowser.ts
+++ src/hooks/useWarmUpBrowser.ts
@@ -1,11 +1,13 @@
 import * as WebBrowser from 'expo-web-browser';
 import React from 'react';
-
+import { Platform } from 'react-native';
 export const useWarmUpBrowser = () => {
   React.useEffect(() => {
-    void WebBrowser.warmUpAsync();
-    return () => {
-      void WebBrowser.coolDownAsync();
-    };
+    if (Platform.OS === 'android') {
+      void WebBrowser.warmUpAsync();
+      return () => {
+        void WebBrowser.coolDownAsync();
+      };
+    }
   }, []);
 };
