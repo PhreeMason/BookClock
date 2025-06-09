3cb21f92f99da6ba53bac96f3758fe7430c37a51
phreemason <mr.omarmason@gmail.com>
2025-06-09 15:01:57 -0400

    fix: update search function to use new 'search-books-v2' endpoint for improved book search functionality

--- src/hooks/useBooks.ts
+++ src/hooks/useBooks.ts
@@ -3,16 +3,15 @@ import { useQuery } from "@tanstack/react-query";
 
 export const useSearchBooks = (query: string) => {
     const client = useSupabase();
-    
     return useQuery({
         queryKey: ['searchBooks', query],
         queryFn: async () => {
-        if (query.length < 4) return null;
-        const { data, error } = await client.functions.invoke('search-books', {
-            body: { query },
-        });
-        if (error) throw new Error(error.message);
-        return data;
+            if (query.length < 4) return null;
+            const { data, error } = await client.functions.invoke('search-books-v2', {
+                body: { query },
+            });
+            if (error) throw new Error(error.message);
+            return data;
         },
         enabled: !!query,
         refetchOnWindowFocus: false,
