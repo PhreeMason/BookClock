22553788c8b46ed0557573f016b15958e92042b6
phreemason <mr.omarmason@gmail.com>
2025-06-11 11:30:11 -0400

    refactor: update ProtectedLayout for book addition screen
    - Remove custom back navigation and related components for a cleaner layout.
    - Change the screen name for adding books to 'book/[api_id]/add' and update header title to 'Add to library'.
    - Simplify loading state handling by retaining the Loader component.

--- src/app/(authenticated)/_layout.tsx
+++ src/app/(authenticated)/_layout.tsx
@@ -1,12 +1,9 @@
 import { Loader } from '@/components/Loader';
-import { IconSymbol } from '@/components/ui/IconSymbol';
 import { useAuth } from '@clerk/clerk-expo';
-import { Redirect, Stack, useRouter } from 'expo-router';
-import { TouchableOpacity } from 'react-native';
+import { Redirect, Stack } from 'expo-router';
 
 export default function ProtectedLayout() {
   const { isSignedIn, isLoaded } = useAuth();
-  const router = useRouter();
 
   if (!isLoaded) {
     return <Loader />;
@@ -20,20 +17,11 @@ export default function ProtectedLayout() {
     <Stack>
       <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
       <Stack.Screen
-        name='book/[api_id]'
+        name='book/[api_id]/add'
         options={{ 
            animation: 'fade_from_bottom',
            presentation: 'modal',
-           headerTitle: 'details',
-           headerLeft: () => (
-            <TouchableOpacity
-              onPress={() => router.back()}
-              style={{
-                padding: 6,
-              }}>
-              <IconSymbol name="arrow.left" size={24} color={'#716E75'} />
-            </TouchableOpacity>
-          ),
+           headerTitle: 'Add to library',
         }}
       />
     </Stack>
