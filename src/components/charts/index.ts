19e7a60d8b20adde8c8b8041ade26df9174aa68a
phreemason <mr.omarmason@gmail.com>
2025-09-01 14:59:07 -0400

    refactor: Remove ProgressAreaChart and SwipeableCharts components from exports

--- src/components/charts/index.ts
+++ src/components/charts/index.ts
@@ -3,5 +3,4 @@ export { default as DailyPagesChart } from './DailyPagesChart';
 export { default as DailyReadingChart } from './DailyReadingChart';
 export { default as DailyReadingProgressChart } from './DailyReadingProgressChart';
 export { default as DeadlineTimelineChart } from './DeadlineTimelineChart';
-export { default as ProgressAreaChart } from './ProgressAreaChart';
 export { default as TotalProgressRingChart } from './TotalProgressRingChart';
--- src/components/features/stats/index.ts
+++ src/components/features/stats/index.ts
@@ -2,5 +2,4 @@ export { default as FormatDistributionChart } from './FormatDistributionChart';
 export { default as ReadingListeningToggle } from './ReadingListeningToggle';
 export { default as ReadingStatsCards } from './ReadingStatsCards';
 export { default as ReadingStreaksChart } from './ReadingStreaksChart';
-export { default as SwipeableCharts } from './SwipeableCharts';
 export { default as WeeklyReadingHeatmap } from './WeeklyReadingHeatmap';
